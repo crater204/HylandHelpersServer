520fdfb359dc9d26d86755dc5557fbb717a726f5
ONBASE\cross <chris.ross@onbase.net>
2018-03-28 12:47:08 -0400

    added basic crud

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ let nextId = 5;
 let users = [ 
     {
         "name" : "Tim",
-        "id" : 0,
+        "id" : "0",
         "favorite-color": "blue"         
     },
     {
         "name" : "Charlie",
-        "id" : 1,
+        "id" : "1",
         "favorite-color": "red"         
     },
     {
         "name" : "Alex",
-        "id" : 2,
+        "id" : "2",
         "favorite-color": "green"         
     },
     {
         "name" : "Bob",
-        "id" : 3,
+        "id" : "3",
         "favorite-color": "aqua"         
     },
     {
         "name" : "Joe",
-        "id" : 4,
+        "id" : "4",
         "favorite-color": "yellow"         
     }
 ];
@@ -37,12 +37,14 @@ app.listen(8000, () => {
     console.log('Server Started');
 });
 
+// Get all users
 app.route('/api/users').get( (req, res) => {
     res.send({
         users
     });
 });
 
+// Get user by id
 app.route('/api/users/:id').get( (req, res) => {
     const userid = req.params['id']
     const filteredUsers = users.filter( (user) => {
@@ -53,11 +55,37 @@ app.route('/api/users/:id').get( (req, res) => {
     });
 });
 
+// Create new user
 app.route('/api/users').post( (req, res) => {
     let reqbody = req.body;
     reqbody.id = nextId;
     users[nextId] = reqbody;
     nextId++;
     
-    res.status(201).send(req.body);
-})
+    res.status(201).send(reqbody);
+});
+
+// Edit user
+app.route('/api/users/:id').put((req, res) => {
+    let reqbody = req.body;
+    let id = req.params['id'];
+    reqbody.id = id;
+    for (var i in users) {
+        if(users[i].id === id) {
+            users[i] = reqbody;
+            break;
+        }
+    }
+
+    res.send(reqbody);
+});
+
+// Delete user
+app.route('/api/users/:id').delete((req, res) => {
+    users = users.filter((user) =>{
+        return user.id != req.params['id'];
+    });
+
+    res.sendStatus(204);
+});
+
